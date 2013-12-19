9f6b5d2d30e9e93c3bcd41a4503b199c58b530a5
TeisD <teis.degreve@gmail.com>
2013-12-19 17:02:01 +0100

    removed useless imports

--- Murderdesk/src/com/egi/interfacelink/InterfaceLink.java
+++ Murderdesk/src/com/egi/interfacelink/InterfaceLink.java
@@ -1,9 +1,7 @@
 package com.egi.interfacelink;
 
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
